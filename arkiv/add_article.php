7422925acdee74fa929acfac994b755262b98c28
jontelarsson94 <jonathan-larsson@outlook.com>
2016-11-10 13:46:27 +0100

    http post

--- arkiv/add_article.php
+++ arkiv/add_article.php
@@ -5,18 +5,22 @@
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <script src="lib/js/angular.min.js"></script>
     <script src="lib/js/jquery.min.js"></script>
+    <script src="ctrl/article.js"></script>
   </head>
-  <body>
+  <body ng-app="article" ng-controller="articleCtrl">
+
+
 
     <div class="section group">
       <div class="col span_2_of_12"></div>
       <div class="col span_8_of_12">
           <div class="col span_12_of_12">
-            <form action="api/add_article.php" method="post">
-                  <input type="text" class="" name="title" id="title">
-                  <textarea rows="5" cols="25" name="body" id="body"></textarea>
+            <form ng-submit="addArticle()">
+                  <input type="text" class="" name="title" id="title" ng-model="articleData.title">
+                  <textarea rows="5" cols="25" name="body" id="body" ng-model="articleData.body">{{articleData.body}}</textarea>
                   <button type="submit" name="submit">Submit</button>
             </form>
+            {{formMessage}}
           </div>
       </div>
       <div class="col span_2_of_12"></div>
--- arkiv/ctrl/article.js
+++ arkiv/ctrl/article.js
@@ -1,9 +1,41 @@
-  var app = angular.module('article', []);
-  app.controller('articleCtrl', function getArticles($scope, $http) {
-      $http.get("api/get_articles.php")
-      .success(function successCallback(response) {
-          $scope.articles = response.result;
-      },function errorCallback(response) {
-          $scope.errors = response.errors;
+angular.module('article', []).controller('articleCtrl', function($scope, $http) {
+  /*For post request we need to have an array with data like the variables below
+  and then ng-model in html to be able to send the data with "data" in angular
+  http method*/
+
+  $scope.articleData = {};
+
+  $scope.getArticles = function (){
+    $http.get("api/get_articles.php")
+    .success(function (response) {
+      if(response.success == true){
+        $scope.articles = response.result;
+        $scope.articles_message = response.message;
+      }else {
+        $scope.articles_error = response.error;
+      }
     });
-  });
+  }
+
+  $scope.addArticle = function() {
+    $http({
+          method  : 'POST',
+          url     : 'api/add_article.php',
+          data    : $.param($scope.articleData),
+          headers : {'Content-Type': 'application/x-www-form-urlencoded'}
+         })
+    .success(function(data) {
+      alert('success');
+        if (!data.success) {
+           //if not successful, bind errors to error variables
+          $scope.errorTitle = data.errors.title;
+          $scope.errorBody = data.errors.body;
+        } else {
+          $scope.formMessageArticle = data.message;
+          $scope.errorArticle = "";
+        }
+      });
+  }
+
+
+});
