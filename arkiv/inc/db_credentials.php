8f52ef2392a3cca2dd07fe37922ed313d5fc5311
jontelarsson94 <jonathan-larsson@outlook.com>
2016-11-30 18:57:50 +0100

    agueli database

--- arkiv/inc/db_credentials.php
+++ arkiv/inc/db_credentials.php
@@ -4,10 +4,19 @@ require_once '../lib/php/medoo.php';
   // Initialize
 $database = new medoo([
     'database_type' => 'mysql',
+    'database_name' => '105649-agueli',
+    'server' => '10.209.1.170',
+    'username' => '105649_lz33499',
+    'password' => 'phujebachi',
+    'charset' => 'utf8'
+]);
+
+/*$database = new medoo([
+    'database_type' => 'mysql',
     'database_name' => 'agueli_arkiv',
     'server' => 'localhost',
     'username' => 'root',
     'password' => 'root',
     'charset' => 'utf8'
-]);
+]);*/
 ?>
