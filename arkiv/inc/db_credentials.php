e4668adf25534e99cb050493341b6684d18a5eba
jontelarsson94 <jonathan-larsson@outlook.com>
2016-12-04 13:14:29 +0100

    -

--- arkiv/inc/db_credentials.php
+++ arkiv/inc/db_credentials.php
@@ -1,36 +1,16 @@
 <?php
 require_once '../lib/php/medoo.php';
 
-/*$testConnection = mysql_connect('10.209.1.170', '105649_lz33499', 'phujebachi');
-if (!$testConnection) {
-die('Error: ' . mysql_error());
-}
-echo 'Database connection working!';
-mysql_close($testConnection);
-*/
-
-/*$database = new medoo([
-    'database_type' => 'mysql',
-    'database_name' => '141364-dva313',
-    'server' => 'mysql18.citynetwork.se',
-    'username' => '141364-wj79764',
-    'password' => 'SolarCalculator',
-    'charset' => 'utf8',
-    'port' => 3306
-]);*/
-/*echo "hej";
   // Initialize
-
-  $database = new medoo([
-      'database_type' => 'mysql',
-      'database_name' => 'vt16_1130ME_sh15hp2475',
-      'server' => '192.168.2.17',
-      'username' => 'sh15hp2475',
-      'password' => 'x7Auvjqm',
-      'charset' => 'utf8',
-      'port' => 3306
-  ]);
-*/
+$database = new medoo([
+    'database_type' => 'mysql',
+    'database_name' => 'vt16_1130ME_sh15hp2475',
+    'server' => '192.168.2.17',
+    'username' => 'sh15hp2475',
+    'password' => 'x7Auvjqm',
+    'charset' => 'utf8'
+]);
+/*
 $database = new medoo([
     'database_type' => 'mysql',
     'database_name' => 'agueli_arkiv',
@@ -38,5 +18,5 @@ $database = new medoo([
     'username' => 'root',
     'password' => 'root',
     'charset' => 'utf8'
-]);
+]);*/
 ?>
--- arkiv/inc/navbar.php
+++ arkiv/inc/navbar.php
@@ -14,9 +14,9 @@
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li <? if($nav_current_page=="articles.php") echo 'class="active "' ?>><a href="articles.php"><h3 class="ag-h3">Artiklar</h3></a></li>
-        <li <? if($nav_current_page=="paintings.php") echo 'class="active "' ?>><a href="paintings.php"><h3 class="ag-h3">Målningar</h3></a></li>
-        <li <? if($nav_current_page=="letters.php") echo 'class="active "' ?>><a href="letters.php"><h3 class="ag-h3">Brev</h3></a></li>
+        <li <? if($nav_current_page=="articles.php") echo 'class="active "' ?>><a href="#"><h3 class="ag-h3">Artiklar</h3></a></li>
+        <li <? if($nav_current_page=="paintings.php") echo 'class="active "' ?>><a href="#"><h3 class="ag-h3">Målningar</h3></a></li>
+        <li <? if($nav_current_page=="letters.php") echo 'class="active "' ?>><a href="#"><h3 class="ag-h3">Brev</h3></a></li>
       </ul>
       <ul class="nav navbar-nav navbar-right">
         <? if($_SESSION['admin'] == 1): ?>
@@ -24,7 +24,6 @@
           <li <? if($nav_current_page=="add_painting.php") echo 'class="active "' ?>><a href="add_painting.php"><h3 class="ag-h3">Lägg till målning</h3></a></li>
           <li <? if($nav_current_page=="add_letter.php") echo 'class="active "' ?>><a href="add_letter.php"><h3 class="ag-h3">Lägg till brev</h3></a></li>
         <? endif;?>
-          <li><a href="logout.php">Logout</a></li>
       </ul>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
