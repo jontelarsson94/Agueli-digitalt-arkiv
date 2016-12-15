0561987365dfbdf9e31cd904edbbaf424de5e04f
jontelarsson94 <jonathan-larsson@outlook.com>
2016-12-15 12:25:06 +0100

    navbar

--- arkiv/inc/navbar.php
+++ arkiv/inc/navbar.php
@@ -14,15 +14,21 @@
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li <? if($nav_current_page=="articles.php") echo 'class="active "' ?>><a href="#"><h3 class="ag-h3">Artiklar</h3></a></li>
+        <li <? if($nav_current_page=="articles.php") echo 'class="active "' ?>><a href="articles.php"><h3 class="ag-h3">Artiklar</h3></a></li>
         <li <? if($nav_current_page=="paintings.php") echo 'class="active "' ?>><a href="#"><h3 class="ag-h3">Målningar</h3></a></li>
         <li <? if($nav_current_page=="letters.php") echo 'class="active "' ?>><a href="#"><h3 class="ag-h3">Brev</h3></a></li>
+        <li <? if($nav_current_page=="add_article.php") echo 'class="active "' ?>><a href="add_article.php"><h3 class="ag-h3">Lägg till artikel</h3></a></li>
+        <li <? if($nav_current_page=="categories.php") echo 'class="active "' ?>><a href="categories.php"><h3 class="ag-h3">Kategorier</h3></a></li>
+        <li <? if($nav_current_page=="articles_admin.php") echo 'class="active "' ?>><a href="articles_admin.php"><h3 class="ag-h3">Artiklar (Admin)</h3></a></li>
       </ul>
       <ul class="nav navbar-nav navbar-right">
         <? if($_SESSION['admin'] == 1): ?>
           <li <? if($nav_current_page=="add_article.php") echo 'class="active "' ?>><a href="add_article.php"><h3 class="ag-h3">Lägg till artikel</h3></a></li>
           <li <? if($nav_current_page=="add_painting.php") echo 'class="active "' ?>><a href="add_painting.php"><h3 class="ag-h3">Lägg till målning</h3></a></li>
           <li <? if($nav_current_page=="add_letter.php") echo 'class="active "' ?>><a href="add_letter.php"><h3 class="ag-h3">Lägg till brev</h3></a></li>
+          <li <? if($nav_current_page=="add_article.php") echo 'class="active "' ?>><a href="#"><h3 class="ag-h3">Lägg till artikel</h3></a></li>
+          <li <? if($nav_current_page=="categories.php") echo 'class="active "' ?>><a href="#"><h3 class="ag-h3">Kategorier</h3></a></li>
+          <li <? if($nav_current_page=="articles_admin.php") echo 'class="active "' ?>><a href="#"><h3 class="ag-h3">Artiklar (Admin)</h3></a></li>
         <? endif;?>
       </ul>
     </div><!-- /.navbar-collapse -->
