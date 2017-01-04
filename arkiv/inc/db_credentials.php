d9f1e9f38a454ca9574cd55f757b717023284f2f
jontelarsson94 <jonathan-larsson@outlook.com>
2017-01-04 18:48:24 +0100

    db connections

--- arkiv/inc/db_credentials.php
+++ arkiv/inc/db_credentials.php
@@ -2,6 +2,7 @@
 require_once '../lib/php/medoo.php';
 
   // Initialize
+//Lindas
 $database = new medoo([
     'database_type' => 'mysql',
     'database_name' => 'vt16_1130ME_sh15hp2475',
@@ -11,12 +12,24 @@ $database = new medoo([
     'charset' => 'utf8'
 ]);
 
+//Localhost
 /*$database = new medoo([
     'database_type' => 'mysql',
     'database_name' => 'agueli_arkiv',
     'server' => 'localhost',
     'username' => 'root',
     'password' => 'root',
+    'charset' => 'utf8'
+]);*/
+
+
+//Agueli
+/*$database = new medoo([
+    'database_type' => 'mysql',
+    'database_name' => '105649-agueli',
+    'server' => 'agueli-105649.mysql.binero.se',
+    'username' => '105649_lz33499',
+    'password' => 'phujebachi',
     'charset' => 'utf8'
 ]);*/
 ?>
